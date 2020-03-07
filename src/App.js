f636b804eb86e1a70e9cfeadd0fd96a90fda62b4
Petr Jelínek <petulinek.jel@gmail.com>
2020-03-07 15:55:28 +0100

    Completed todo list

--- src/App.js
+++ src/App.js
@@ -1,19 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Layout from "./components/Layout/Layout";
 import Wrapper from "./components/Wrapper/Wrapper";
 import Task from "./components/Task/Task";
 
+import tasksArray from "../src/data/tasks.json";
+
 function App() {
+  const [newTask, setNewTask] = useState("");
+  const [tasks, setTasks] = useState(tasksArray);
+  const [editing, setEditing] = useState(null);
+  const [newValue, setNewValue] = useState("");
+
+  const renderTasks = () =>
+    tasks.map(({ id, task }) => (
+      <Task
+        key={id}
+        task={task}
+        id={id}
+        deleteTask={() => deleteTask(id)}
+        editing={editing}
+        setEditing={setEditing}
+        editTask={() => editTask(id)}
+        newValue={newValue}
+        setNewValue={setNewValue}
+        saveEditing={() => saveEditing(newValue, id)}
+      ></Task>
+    ));
+
+  const renderNone = () => <h3 className="no-items">No items here 😥</h3>;
+
+  const addTask = event => {
+    if (event.key === "Enter") {
+      if (!newTask) return;
+
+      const newTaskObject = {
+        id: Date.now(),
+        task: newTask
+      };
+
+      setTasks([newTaskObject, ...tasks]);
+      setNewTask("");
+    }
+  };
+
+  const deleteTask = id => {
+    const filteredTasks = tasks.filter(task => task.id !== id);
+    setTasks(filteredTasks);
+  };
+
+  const editTask = id => {
+    setEditing(id);
+  };
+
+  const saveEditing = (newVal, id) => {
+    const oldTask = tasks.find(task => task.id === id);
+
+    if (!newVal) return;
+    oldTask.task = newVal;
+  };
+
   return (
     <Layout>
       <h2 className="title">To-Do</h2>
       <Wrapper>
-        <input className="input"></input>
-        <button className="add-button">+</button>
+        <input
+          onChange={e => setNewTask(e.target.value)}
+          value={newTask}
+          name="task"
+          className="input"
+          onKeyPress={addTask}
+        ></input>
       </Wrapper>
       <h2 className="task-title">Tasks</h2>
-      <Task name="Test"></Task>
+      {tasks.length === 0 ? renderNone() : renderTasks()}
     </Layout>
   );
 }
--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,13 +1,57 @@
 import React from "react";
 import "./Task.css";
 
-const Task = ({ name }) => {
-  return (
-    <div className="card-wrapper">
-      <h4 className="card-title">{name}</h4>
-      <span className="delete-task">Delete</span>
-    </div>
-  );
+const Task = ({
+  task,
+  id,
+  deleteTask,
+  editing,
+  setEditing,
+  editTask,
+  newValue,
+  setNewValue,
+  saveEditing
+}) => {
+  const read = () => {
+    return (
+      <div className="card-wrapper">
+        <h4 className="card-title">{task}</h4>
+        <div className="icons-wrapper">
+          <span className="delete-task" onClick={deleteTask}>
+            🗑️
+          </span>
+          <span className="edit-task" onClick={editTask}>
+            🖊️
+          </span>
+        </div>
+      </div>
+    );
+  };
+
+  const edit = () => {
+    return (
+      <div className="card-wrapper">
+        <input
+          className="edit-input"
+          value={newValue}
+          name="edit"
+          onChange={e => setNewValue(e.target.value)}
+        ></input>
+        <span
+          className="save-task"
+          onClick={() => {
+            saveEditing(newValue, id);
+            setEditing(null);
+            setNewValue("");
+          }}
+        >
+          ✔️
+        </span>
+      </div>
+    );
+  };
+
+  return editing === id ? edit() : read();
 };
 
 export default Task;
